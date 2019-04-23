4e31688981cfc6a1312d556afe69650655534cd3
tr00per <tr00per4@mail.ru>
2019-04-23 15:45:57 +0300

    my new js

---
+++ config/config.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql')
+
+let pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database:process.env.DB_DATABASE
+
+})
+
+module.exports = pool
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const pool = require('./config/config')
+
+
+console.log(process.env)
 
 app.route('/').get((request, response) => {
     response.send('Hello world')
 })
 
-app.route('/:group').get((request, response) => {
+app.route('/api/t/:group').get((request, response) => {
     let group = request.params["group"]
     response.send(`Hello ${group}`)
 })
+
+app.route('/api/students').get((req, res) => {
+    pool.query('Select * from students', (err, result, fields) => {
+       if(err) throw err
+        res.send(result)
+    })
+})
+
 
 app.listen(8080, () => {
        console.log('Server started')
